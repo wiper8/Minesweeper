951719e494ee87390aa427a3aa690cc268d0f469
William Perron <wil.perron@gmail.com>
2026-01-14 21:34:13 -0500

    is_game_over

---
+++ src/game_engine/is_game_over.R
@@ -0,0 +1,36 @@
+source("hp.R")
+
+#' Est-ce que la partie est perdue?
+#'
+#' @param grid matrice de minesweeper
+#'
+#' @returns nombre entier : -1 si la partie est perdue, 1 si la partie est gagnée, 0 si la partie est en cours
+#' @export
+#'
+#' @examples
+is_game_over <- function(grid, verbose = FALSE) {
+  verbo <- function(grid, verbose) {
+    if (verbose) {
+      print(grid)
+    }
+  }
+  
+  if (any(grid == uncovered_mine)) {
+    verbo(grid, verbose)
+    return(-1)
+  }
+  
+  # erreur de drapeau : pour un humain c'est possible, mais pour mon ordi, ça ne devrait pas l'être. Lancer le débogueur
+  # dans un tel cas pour l'instant
+  if (any(grid == flag_on_no_mine)) { # TODO retirer éventuellement pour permettre à un humain de jouer
+    verbo(grid, verbose)
+    browser()
+  }
+  
+  # toutes les boîtes sans mines sont cliquées
+  if (sum(grid %in% c(covered_no_mine, uncovered_no_mine, uncovered_mine, flag_on_no_mine)) == 0) {
+    verbo(grid, verbose)
+    return(1)
+  }
+  0
+}
