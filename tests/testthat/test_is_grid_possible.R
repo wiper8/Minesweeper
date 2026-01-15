11fd1f7449ca7fd1551f19637cb636d38dd71d84
William Perron <wil.perron@gmail.com>
2026-01-14 21:16:05 -0500

    is_grid_possible

---
+++ tests/testthat/test_is_grid_possible.R
@@ -0,0 +1,55 @@
+test_that("is_grid_possible retourne faux pour des nombres qui supposerait trop de mines", {
+  expect_false(
+    is_grid_possible(matrix(c(-1, -2, 2, -1), nrow = 2))
+  )
+})
+
+test_that("is_grid_possible retourne faux pour des nombres qui supposerait trop peu de mines", {
+  expect_false(
+    is_grid_possible(matrix(c(-1, -1, -1, -5, 0, -1, -1, -1, -1), nrow = 3))
+  )
+  expect_false(
+    is_grid_possible(matrix(c(-1, -2, -1, -2, 1, -1, -1, -1, -1), nrow = 3))
+  )
+})
+
+test_that("is_grid_possible retourne les bonnes valeurs selon tous les hyperparamètres possibles", {
+  base_grid <- matrix(c(-1:-6), nrow = 3, ncol = 4)
+  expect_false(all(sapply(
+    c(0:2, 4:8),
+    function(number) {
+      grid <- base_grid
+      grid[1] <- number
+      is_grid_possible(grid)
+    }
+  )))
+  grid <- base_grid
+  grid[1] <- 3
+  expect_true(is_grid_possible(grid))
+  
+  expect_false(all(sapply(
+    c(0:1, 3:8),
+    function(number) {
+      grid <- base_grid
+      grid[2] <- number
+      is_grid_possible(grid)
+    }
+  )))
+  grid <- base_grid
+  grid[2] <- 2
+  expect_true(is_grid_possible(grid))
+  
+  expect_false(all(sapply(
+    c(0:1, 3:8),
+    function(number) {
+      grid <- base_grid
+      grid[3] <- number
+      is_grid_possible(grid)
+    }
+  )))
+  grid <- base_grid
+  grid[3] <- 2
+  expect_true(is_grid_possible(grid))
+
+  # TODO peut-être en rajouter
+})
