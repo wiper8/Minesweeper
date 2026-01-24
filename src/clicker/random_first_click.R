854651e750bd1cb4735fb0584a389e1f8e4ffa38
William Perron <wil.perron@gmail.com>
2026-01-24 15:00:37 -0500

    ajout random clicker, simulationd e partie

---
+++ src/clicker/random_first_click.R
@@ -0,0 +1,3 @@
+random_first_click <- function(dims) {
+  c(sample(1:dims[1], 1), sample(1:dims[2], 1))
+}
--- src/compute_probs_success.R
+++ src/compute_probs_success.R
@@ -1,10 +1,11 @@
-source("src/simulate_a_game.R")
+source("src/simulate_game.R")
 source("src/plots.R")
 
 compute_probs_success <- function(n, mines, dims = c(17, 9), clicker) {
-  x <- replicate(n, simulate_a_game(mines, dims, clicker))
+  all_simuls <- replicate(n, simulate_game(mines, dims, clicker), simplify = FALSE)
+  wins <- sapply(all_simuls, function(lst) lst[[2]] == "win")
   list(
-    mean = mean(x),
-    interval = prob_interval(sum(x), n)
+    mean = mean(wins),
+    interval = prob_interval(sum(wins), n)
   )
 }
