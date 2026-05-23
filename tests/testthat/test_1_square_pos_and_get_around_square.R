9a676004b61ea870cb523ab02c208cedffa9466d
William Perron <wil.perron@gmail.com>
2026-05-23 14:59:55 -0400

    unname dans un test

--- tests/testthat/test_1_square_pos_and_get_around_square.R
+++ tests/testthat/test_1_square_pos_and_get_around_square.R
@@ -1,8 +1,10 @@
+source("src/indicies/square_pos_and_get_around_square.R")
+
 test_that("square_pos_and_get_around_square works", {
   expect_equal(
     square_pos_and_get_around_square(c(4, 2), matrix(1:12, ncol = 3)),
     list(
-      expand.grid(3:4, 1:3) |> as.matrix(),
+      expand.grid(3:4, 1:3) |> as.matrix() |> unname(),
       matrix(c(3:4, 7:8, 11:12), ncol = 3)
     )
   )
