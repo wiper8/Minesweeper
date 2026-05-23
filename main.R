c98e9ecd696dcce2588cc2c5bf95bd7e62bb2d05
William Perron <wil.perron@gmail.com>
2026-05-23 14:22:04 -0400

    ajout d'un TODO

--- main.R
+++ main.R
@@ -19,6 +19,10 @@ set.seed(2026L)
 show_mines_difficulty(compare_clickers(n = 200, c(6, 4)))
 show_mines_difficulty(compare_clickers(n = 200, c(17, 9)))
 
+# TODO aussi évaluer le temps pour résoudre. Car je souhaite trouver l'espérance de temps avant une première solution 
+# avec n mines, donc parfois plus de mines = essais plus courts = plus d'essais = pas nécessairement plus long
+# si p = 2% avec t=3min, vs p=1% avec t=1min, 1/2% * 3 > 1/1% * 1
+
 hypothesis_test(20, total_mines = 10, dims = c(17, 9), random_clicker, certain_else_random_clicker)
 hypothesis_test(50, total_mines = 56, dims = c(17, 9), certain_else_random_clicker, human_clicker)
 
