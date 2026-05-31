6cbb4dd4da397c3b40e329b76d832d89e59c2398
William Perron <wil.perron@gmail.com>
2026-05-31 13:11:03 -0400

    préparation de smart_clicker

---
+++ src/clicker/smart_clicker.R
@@ -0,0 +1,17 @@
+source("src/clicker/certain_core.R")
+source("src/clicker/random_clicker.R")
+source("src/clicker/probabilistic_clicker.R")
+
+smart_clicker <- function(grid, mines_left, ...) {
+  tmp <- certain_core(grid, mines_left, ...)
+  if (!is.null(tmp)) {
+    return(tmp)
+  }
+  browser()
+  tmp <- probabilistic_clicker(grid, mines_left = mines_left, ...)
+  if (!is.null(tmp)) {
+    return(tmp)
+  }
+  # si aucune stratégie
+  browser() # pas sensé ce rendre ici
+}
